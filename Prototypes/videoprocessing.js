eac045e8d6609424e097afc022f48217166699d3
NMStiegler <noahstieg@gmail.com>
2022-03-30 01:20:19 -0400

    Improved formatting and commenting, added another function, and fixed double greyscale bug within addStep function

--- Prototypes/videoprocessing.js
+++ Prototypes/videoprocessing.js
@@ -1,15 +1,46 @@
+/**
+ * TODO: Show function queue on HTML page
+ * - set innerHTML to show what's in the function queue object
+ *
+ * TODO: Do all the generating code stuff
+ * - function that generates applies processing in order user queues
+ * - function returns what it's supposed to
+ * - et cetera
+ *
+ * TODO: Add more functions:
+ * - a threshold you can choose color & value with a slider
+ * - a way to find objects in scene
+ *   - contours
+ *   - min enclosing circles within threshold min/max
+ *   - # of objects
+ *   - positions of objects
+ * - eventually code where user can do something if object is within an rect
+ *
+ * TODO: css & making the site look pretty
+ * - Backgrounds to differentiate the sections
+ * - Placement
+ * - Buttons and images sized for visibility / accessibility
+ * - how to use the page section
+ */
+
 // Global object to track which order functions should run in
 let functionQueue = {
     numFunctions: 0,
     functions: [],
 };
 
-let possibleFunctions = ["threshold"];
+// Global unique greyscale function because we can only have one
+let greyscale = { name: "greyscale", parameters: [] };
 
 // global variables to hold the SIZE of the input
 var input_width = 320;
 var input_height = 240;
 
+// global variables to hold the SIZE of the output
+var output_width = 320;
+var output_height = 240;
+
+// Function from Prof Danahy to start streaming video (chrome only?)
 function start_video(video_id) {
     var video_canvas = document.getElementById(video_id);
     // Set the width and height:
@@ -27,11 +58,7 @@ function start_video(video_id) {
     }
 }
 
-// global variables to hold the SIZE of the output
-var output_width = 320;
-var output_height = 240;
-
-// takes video data and displays on a canvas
+// takes video data and displays on a canvas, also can return frame as img
 function display_frame(src_canvas_id, dst_canvas_id) {
     // source canvas
     var src_canvas = document.getElementById(src_canvas_id);
@@ -47,19 +74,23 @@ function display_frame(src_canvas_id, dst_canvas_id) {
     // return the image data
     return cv.imread(dst_canvas_id);
 }
-//Holds repeated processing
+
+// Holder variable for interval of repeated processing
 var process;
 
+// Clears the interval variable
 function resetProcessing() {
     clearInterval(process);
 }
 
+// Sets interval variable with function and interval timing
 function repeatProcess(src_id, dest_id) {
     resetProcessing();
     var tempo = document.getElementById("tempo").value;
     process = setInterval(doProcess, tempo, src_id, dest_id);
 }
 
+// Iterates through each processing step before showing final image
 function doProcess(src_id, dest_id) {
     // Read image from the video stream
     var img = display_frame(src_id, dest_id);
@@ -73,23 +104,37 @@ function doProcess(src_id, dest_id) {
     img.delete();
 }
 
-function addStep(funcName) {
-    functionQueue.numFunctions++;
-    functionQueue.functions.push(funcName);
-    console.log("Added", funcName);
+// Add step to the function processing queue
+// Won't add greyscale twice
+function addStep(funcObject) {
+    if (funcObject["name"] != "greyscale") {
+        functionQueue.numFunctions++;
+        functionQueue.functions.push(funcObject);
+        console.log("Added", funcObject["name"]);
+    } else if (!functionQueue.functions.includes(greyscale)) {
+        functionQueue.numFunctions++;
+        functionQueue.functions.push(greyscale);
+        console.log("Added", greyscale["name"]);
+    }
 }
 
+// Removes step from function processing queue
 function removeStep() {
     if (functionQueue.numFunctions > 0) {
         functionQueue.numFunctions--;
-        console.log("Removed:", functionQueue.functions.pop());
+        console.log("Removed:", functionQueue.functions.pop()["name"]);
     }
 }
 
-// Series of if statements find the right function and apply it to the image
-function doProcessingStep(funcName, img) {
-    if (funcName == "threshold") {
-        cv.threshold(img, img, 128, 255, cv.THRESH_BINARY);
+// Finds the right processing step and executes it
+function doProcessingStep(funcObject, img) {
+    funcName = funcObject["name"];
+    switch (funcName) {
+        case "threshold":
+            cv.threshold(img, img, 128, 255, cv.THRESH_BINARY);
+            break;
+        case "greyscale":
+            cv.cvtColor(img, img, cv.COLOR_RGBA2GRAY);
+            break;
     }
-    // return img; // needed?
 }
