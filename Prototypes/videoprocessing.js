5265e0c8e39c3d4279595894e09b3aa146577243
NMStiegler <noahstieg@gmail.com>
2022-04-05 18:18:34 -0400

    Refactored every video processing function into its own class

--- Prototypes/videoprocessing.js
+++ Prototypes/videoprocessing.js
@@ -1,14 +1,24 @@
 /**
- * TODO: Show function queue on HTML page
- * - set innerHTML to show what's in the function queue object
+ * TODO: Make functions classes
+ * - Put classes in separate file as modules & export them
+ *      - each class should have an .execute(), a .render(), a .showCode()
+ * - Import classes from separate files (in folder) into main js file
+ * - Have each module reference an html file (in html folder) that has html for
+ *   the settings box for that class
+ *
+ * TODO: Improve functionality of FunctionQueue
+ * - Swap two functions
+ * - Delete function at index
+ * - Insert function at index
+ * -
  *
  * TODO: Do all the generating code stuff
- * - function that generates applies processing in order user queues
- * - function returns what it's supposed to
- * - et cetera
+ * - function that runs through queue and calls .showCode()
+ * - makes sure parameters etc. are linked up
+ * - have library of functions for copy/paste?
  *
  * TODO: Add more functions:
- * - a threshold you can choose color & value with a slider
+ * - Threshold: make RGB/All not text box, make value a slider
  * - a way to find objects in scene
  *   - contours
  *   - min enclosing circles within threshold min/max
@@ -30,14 +40,134 @@
  */
 
 // Template for all of our functions
-class CvFunction {
-    constructor(name, id, params, outputs, chngimg) {
-        this.name = name;
+class Greyscale {
+    constructor(id) {
+        this.name = "greyscale";
         this.id = id;
-        this.params = params;
-        this.outputs = outputs;
-        // Boolean value if the function changes the image or not
-        this.chngimg = chngimg;
+    }
+
+    execute(img) {
+        cv.cvtColor(img, img, cv.COLOR_RGBA2GRAY);
+    }
+
+    // Returns HTML that make a greyscale block to show on queue
+    renderHTML() {
+        let html = "<div id='" + this.id + "HTMLdiv" + "'><blockquote>";
+        html += "<p> ID: " + this.id + " Name: " + this.name + " </p>";
+        html += "</p>";
+        html += "</blockquote></div>";
+        return html;
+    }
+}
+
+class Threshold {
+    constructor(id) {
+        this.name = "threshold";
+        this.id = id;
+        this.params = {
+            color: "all",
+            value: "100",
+        };
+    }
+
+    get color() {
+        return this.params.color;
+    }
+
+    get value() {
+        return this.params.value;
+    }
+
+    execute(img) {
+        let color = this.color;
+        let threshold = Number(this.value);
+        if (
+            color != "all" &&
+            color != "red" &&
+            color != "blue" &&
+            color != "green"
+        ) {
+            // console.log("incorrect color");
+            return;
+        }
+        if (color == "all") {
+            cv.threshold(img, img, threshold, 255, cv.THRESH_BINARY);
+        } else {
+            for (var i = 0; i < img.data.length; i += 4) {
+                var r = img.data[i]; // red
+                var g = img.data[i + 1]; // green
+                var b = img.data[i + 2]; // blue
+                var a = img.data[i + 3]; // alpha
+                if (color == "red" && r - g > threshold && r - b > threshold) {
+                    // pixel is very red, so leave it
+                } else if (
+                    color == "green" &&
+                    g - r > threshold &&
+                    g - b > threshold
+                ) {
+                    // Pixel is very green so do nothing
+                } else if (
+                    color == "blue" &&
+                    b - r > threshold &&
+                    b - g > threshold
+                ) {
+                    // Pixel is very blue so do nothing
+                } else {
+                    // pixel is NOT very much the color we want, so set to black
+                    img.data[i] = 0;
+                    img.data[i + 1] = 0;
+                    img.data[i + 2] = 0;
+                }
+            }
+        }
+    }
+
+    // TODO: sloppy way - xttp requests later. Just have .render()
+    renderHTML() {
+        let html = "<div id='" + this.id + "HTMLdiv" + "'><blockquote>";
+        html += "<p> ID: " + this.id + " Name: " + this.name + " </p>";
+
+        // Color value
+        html += '<input type="text" id="' + this.id + "color" + '"';
+        html += ' name="' + this.id + "color" + '" value="' + this.color + '">';
+        html +=
+            '<label for="' +
+            this.id +
+            "thresh" +
+            '"> Color: red, green, blue, or all </label><br>';
+
+        // Thresh value
+        html += '<input type="text" id="' + this.id + "thresh" + '"';
+        html +=
+            ' name="' + this.id + "thresh" + '" value="' + this.value + '">';
+        html +=
+            '<label for="' + this.id + "thresh" + '"> Threshold </label><br>';
+
+        html += "</p>";
+        html += "</blockquote></div>";
+        return html;
+    }
+
+    renderJS() {
+        let script = "";
+
+        // Listener for change in threshold value
+        script += "var thresholdValue" + this.id + " = ";
+        script += 'document.getElementById("' + this.id + "thresh" + '"); ';
+        script += "thresholdValue" + this.id + ".oninput = function () {";
+        script += "functionQueue.functionWithID('" + this.id + "')";
+        script += ".params.value = this.value";
+        script += ";}; ";
+
+        // Listener for change in color value
+        script += "var colorValue" + this.id + " = ";
+        script += 'document.getElementById("' + this.id + "color" + '");';
+        script += "colorValue" + this.id + ".oninput = function () {";
+        script += "functionQueue.functionWithID('" + this.id + "')";
+        script += ".params.color = this.value";
+        script += ";}; ";
+
+        return script;
     }
 }
 
@@ -70,26 +200,27 @@ class FunctionQueue {
 
     // Add step to the function processing queue
     // Won't add greyscale twice
-    add(name, params, outputs, chngimg) {
-        if (name != "greyscale") {
-            let temp = new CvFunction(
-                name,
-                "ID" + this.id_gen_seed,
-                params,
-                outputs,
-                chngimg
-            );
-            this.functions["func" + this.length] = temp;
-            this.length++;
-            this.id_gen_seed++;
-            console.log("Added", name);
-        } else if (!functionQueue.includes_greyscale) {
-            this.functions["func" + this.length] = greyscale;
-            this.length++;
-            this.id_gen_seed++;
-            this.includes_greyscale = true;
-            console.log("Added", greyscale["name"]);
+    add(name) {
+        let temp = {};
+        switch (name) {
+            case "threshold":
+                temp = new Threshold("ID" + this.id_gen_seed);
+                break;
+            case "greyscale":
+                if (!this.includes_greyscale) {
+                    temp = new Greyscale("ID" + this.id_gen_seed);
+                    this.includes_greyscale = true;
+                } else {
+                    console.log("Can't greyscale more than once");
+                    return;
+                }
+                break;
         }
+        this.functions["func" + this.length] = temp;
+        this.length++;
+        this.id_gen_seed++;
+        console.log("Added", name);
+        this.showQueue("visibleQueue", "queueScripts");
     }
 
     // Takes last function off queue and returns a copy of it
@@ -100,10 +231,18 @@ class FunctionQueue {
         );
         delete this.functions["func" + (this.length - 1)];
         this.length--;
-        if (temp.name == greyscale.name) {
+        if (temp.name == "greyscale") {
             this.includes_greyscale = false;
         }
+        this.showQueue("visibleQueue", "queueScripts");
         return temp;
+    }
+
+    // Removes step from function processing queue
+    removeStep() {
+        if (this.len > 0) {
+            console.log("Removed:", this.pop()["name"]);
+        }
     }
 
     // Returns the function with the given ID
@@ -116,12 +255,45 @@ class FunctionQueue {
         }
         console.log("No function found with id", id);
     }
+
+    //TODO: only takes html div, since scripts shouldn't be needed
+    // Updates the visual queue with latest from functionQueue
+    // Also runs scripts which enable page content to modify functionQueue
+    showQueue(htmlDivID, jsDivID) {
+        let html = "";
+        let scripts = "";
+        for (let i = 0; i < this.len; i++) {
+            let func = this.function_at(i);
+            html += func.renderHTML();
+            try {
+                scripts += func.renderJS();
+            } catch {}
+            html += "<br>";
+        }
+
+        // Removes all lingering scripts
+        let element = document.getElementById(jsDivID);
+        while (element.firstChild) {
+            element.removeChild(element.firstChild);
+        }
+
+        // Adds new html & scripts & runs them
+        let newScript = document.createElement("script");
+        newScript.innerHTML = scripts;
+        newScript.id = "ID" + functionQueue.id_gen_seed;
+        document.getElementById(htmlDivID).innerHTML = html;
+        document.getElementById(jsDivID).appendChild(newScript);
+    }
 }
 
-let functionQueue = new FunctionQueue();
+/*
+ *
+ * Global Vars
+ *
+ */
 
-// Global unique greyscale function because we can only have one
-const greyscale = new CvFunction("greyscale", "IDgreyscale", {}, {}, true);
+let functionQueue = new FunctionQueue();
+let processingFunctions = ["threshold", "greyscale"];
 
 // global variables to hold the SIZE of the input
 var input_width = 320;
@@ -187,120 +359,13 @@ function repeatProcess(src_id, dest_id) {
     process = setInterval(doProcess, tempo, src_id, dest_id);
 }
 
-/*
- *
- * Show Queue Functions
- *
- */
-
-// Updates the visual queue with latest from functionQueue
-// Also runs scripts which enable page content to modify functionQueue
-function showQueue() {
-    let html = "";
-    let scripts = "";
-    for (let i = 0; i < functionQueue.len; i++) {
-        func = functionQueue.function_at(i);
-        if (func.name == "threshold") {
-            html += makeThresholdBlock(func);
-            scripts += makeThresholdScripts(func);
-        } else if (func.name == "greyscale") {
-            html += makeGreyscaleBlock(func);
-        }
-        html += "<br>";
-    }
-
-    // Removes all lingering scripts
-    let element = document.getElementById("queueScripts");
-    while (element.firstChild) {
-        element.removeChild(element.firstChild);
-    }
-
-    // Adds new html & scripts & runs them
-    let newScript = document.createElement("script");
-    newScript.innerHTML = scripts;
-    newScript.id = "ID" + functionQueue.id_gen_seed;
-    document.getElementById("visibleQueue").innerHTML = html;
-    document.getElementById("queueScripts").appendChild(newScript);
-}
-
-// Makes visual queue block for a threshold
-function makeThresholdBlock(func) {
-    let html = "<div id='" + func.id + "HTMLdiv" + "'><blockquote>";
-    html += "<p> ID: " + func.id + " Name: " + func.name + " </p>";
-    // Color picker
-    html += "<p> Color: </p>";
-
-    // Color value
-    html += '<input type="text" id="' + func.id + "color" + '"';
-    html +=
-        ' name="' + func.id + "color" + '" value="' + func.params.color + '">';
-    html +=
-        '<label for="' +
-        func.id +
-        "thresh" +
-        '"> Color: red, green, blue, or all </label><br>';
-
-    // Thresh value
-    html += '<input type="text" id="' + func.id + "thresh" + '"';
-    html +=
-        ' name="' +
-        func.id +
-        "thresh" +
-        '" value="' +
-        func.params.threshValue +
-        '">';
-    html += '<label for="' + func.id + "thresh" + '"> Threshold </label><br>';
-
-    html += "</p>";
-    html += "</blockquote></div>";
-    return html;
-}
-
-// Returns scripts that make a threshold block interactive
-function makeThresholdScripts(func) {
-    let script = "";
-
-    // Listener for change in threshold value
-    script += "var thresholdValue" + func.id + " = ";
-    script += 'document.getElementById("' + func.id + "thresh" + '"); ';
-    script += "thresholdValue" + func.id + ".oninput = function () {";
-    script += "functionQueue.functionWithID('" + func.id + "')";
-    script += ".params.threshValue = this.value";
-    script += ";}; ";
-
-    // Listener for change in color value
-    script += "var colorValue" + func.id + " = ";
-    script += 'document.getElementById("' + func.id + "color" + '");';
-    script += "colorValue" + func.id + ".oninput = function () {";
-    script += "functionQueue.functionWithID('" + func.id + "')";
-    script += ".params.color = this.value";
-    script += ";}; ";
-
-    return script;
-}
-
-// Returns HTML that make a greyscale block to show on queue
-function makeGreyscaleBlock(func) {
-    let html = "<div id='" + func.id + "HTMLdiv" + "'><blockquote>";
-    html += "<p> ID: " + func.id + " Name: " + func.name + " </p>";
-    html += "</p>";
-    html += "</blockquote></div>";
-    return html;
-}
-
-/*
- *
- * Video Processing Functions
- *
- */
-
 // Iterates through each processing step before showing final image
 function doProcess(src_id, dest_id) {
     // Read image from the video stream
     var img = display_frame(src_id, dest_id);
 
     for (let i = 0; i < functionQueue.len; i++) {
-        doProcessingStep(functionQueue.function_at(i), img);
+        functionQueue.function_at(i).execute(img);
     }
 
     // Show final image
@@ -308,72 +373,22 @@ function doProcess(src_id, dest_id) {
     img.delete();
 }
 
-// Removes step from function processing queue
-function removeStep() {
-    if (functionQueue.len > 0) {
-        console.log("Removed:", functionQueue.pop()["name"]);
-    }
-}
-
-// Finds the right processing step and executes it
-function doProcessingStep(funcObject, img) {
-    funcName = funcObject["name"];
-    switch (funcName) {
-        case "threshold":
-            doThreshold(funcObject, img);
-            break;
-        case "greyscale":
-            cv.cvtColor(img, img, cv.COLOR_RGBA2GRAY);
-            break;
-    }
-}
-
 /*
  *
- * OpenCV Implementations
+ * Page Generating Functions
  *
  */
 
-function doThreshold(funcObject, img) {
-    let color = funcObject.params.color;
-    let threshold = Number(funcObject.params.threshValue);
-    if (
-        color != "all" &&
-        color != "red" &&
-        color != "blue" &&
-        color != "green"
-    ) {
-        // console.log("incorrect color");
-        return;
-    }
-    if (color == "all") {
-        cv.threshold(img, img, threshold, 255, cv.THRESH_BINARY);
-    } else {
-        for (var i = 0; i < img.data.length; i += 4) {
-            var r = img.data[i]; // red
-            var g = img.data[i + 1]; // green
-            var b = img.data[i + 2]; // blue
-            var a = img.data[i + 3]; // alpha
-            if (color == "red" && r - g > threshold && r - b > threshold) {
-                // pixel is very red, so leave it
-            } else if (
-                color == "green" &&
-                g - r > threshold &&
-                g - b > threshold
-            ) {
-                // Pixel is very green so do nothing
-            } else if (
-                color == "blue" &&
-                b - r > threshold &&
-                b - g > threshold
-            ) {
-                // Pixel is very blue so do nothing
-            } else {
-                // pixel is NOT very much the color we want, so set to black
-                img.data[i] = 0;
-                img.data[i + 1] = 0;
-                img.data[i + 2] = 0;
-            }
-        }
+function addButtons() {
+    console.log("adding buttons");
+    let buttonDiv = document.getElementById("buttons");
+    for (let i = 0; i < processingFunctions.length; i++) {
+        let func = processingFunctions[i];
+        let button = document.createElement("button");
+        button.innerHTML = func;
+        button.onclick = function () {
+            functionQueue.add(func);
+        };
+        buttonDiv.appendChild(button);
     }
 }
